bd09e1ae76dd536f09ff899e1d8e77b27b7ebe26
TangZhong <love007zhong@163.com>
2015-08-11 11:12:36 +0800

    用于短信验证时计算倒计时的计算器。

---
+++ KFAGCDForTime/KFAGCDForTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KFAGCDForTime
+//
+//  Created by TangZhong on 15-8-10.
+//  Copyright (c) 2015年 KeFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
