984eab643b427da3ff19a5ee40d6c4cf1eb372cb
KFAaron <love007zhong@163.com>
2016-04-15 11:11:12 +0800

    update

--- KFACoreAnimationDemo/KFACoreAnimationDemo/KFAAnchorPointTestViewController.m
+++ KFACoreAnimationDemo/KFACoreAnimationDemo/KFAAnchorPointTestViewController.m
@@ -16,6 +16,8 @@
 @property (weak, nonatomic) IBOutlet UIImageView *secondImage;
 @property (nonatomic, weak) NSTimer *timer;
 
+@property (strong, nonatomic) IBOutletCollection(UIView) NSArray *timeViewArr;
+
 @end
 
 @implementation KFAAnchorPointTestViewController
@@ -28,6 +30,7 @@
     [super viewDidLoad];
     
     [self setImageViewAnchorPoint];
+    [self initTimeView];
     
     @weakify(self);
     self.timer = [NSTimer kfa_scheduledTimerWithTimeInterval:1.0 block:^{
@@ -45,16 +48,39 @@
     self.secondImage.layer.anchorPoint = CGPointMake(0.5f, 0.9f);
 }
 
+- (void)initTimeView {
+    UIImage *digitImage = [UIImage imageNamed:@"num"];
+    for (UIView *view in self.timeViewArr) {
+        view.layer.contents = (__bridge id)digitImage.CGImage;
+        view.layer.contentsRect = CGRectMake(0, 0, 0.1, 1.0);
+        view.layer.contentsGravity = kCAGravityResizeAspect;
+        // 设置图片的拉伸过滤方式 默认kCAFilterLinear
+//        view.layer.magnificationFilter = kCAFilterNearest;
+    }
+}
+
 - (void)tick {
     NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     NSUInteger units = NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
     NSDateComponents *components = [calendar components:units fromDate:[NSDate date]];
+    
     CGFloat hourAngle = (components.hour/12.0)*M_PI*2.0;
     CGFloat minuteAngle = (components.minute/60.0)*M_PI*2.0;
     CGFloat secondAngle = (components.second/60.0)*M_PI*2.0;
     self.hourImage.transform = CGAffineTransformMakeRotation(hourAngle);
     self.minuteImage.transform = CGAffineTransformMakeRotation(minuteAngle);
     self.secondImage.transform = CGAffineTransformMakeRotation(secondAngle);
+    
+    [self setDigit:(components.hour/10) forView:self.timeViewArr[0]];
+    [self setDigit:(components.hour%10) forView:self.timeViewArr[1]];
+    [self setDigit:(components.minute/10) forView:self.timeViewArr[2]];
+    [self setDigit:(components.minute%10) forView:self.timeViewArr[3]];
+    [self setDigit:(components.second/10) forView:self.timeViewArr[4]];
+    [self setDigit:(components.second%10) forView:self.timeViewArr[5]];
+}
+
+- (void)setDigit:(NSInteger)digit forView:(UIView *)view {
+    view.layer.contentsRect = CGRectMake(digit*0.1, 0, 0.1, 1.0);
 }
 
 - (void)didReceiveMemoryWarning {
